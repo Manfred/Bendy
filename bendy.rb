270f2f1cdb6fe49d24f54af11e68e6c669a47846
Manfred Stienstra <manfred@fngtps.com>
2010-12-03 08:53:54 +0100

    Remove unnecessary temporary variable.

--- bendy.rb
+++ bendy.rb
@@ -1,9 +1,8 @@
 module Bendy
   module Shape
     def shape(*args)
-      klass      = args[0].kind_of?(Hash) ? Class.new : args.shift
-      attributes = args[0]
-      attributes.each do |attribute, value|
+      klass = args[0].kind_of?(Hash) ? Class.new : args.shift
+      args[0].each do |attribute, value|
         proc = value.kind_of?(Proc) ? value : lambda { value }
         klass.send(:define_method, attribute, &proc)
       end
