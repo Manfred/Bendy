9ebdc68df2f3462dc11caca0b2954d2a655b7f38
Manfred Stienstra <manfred@fngtps.com>
2013-12-19 22:27:43 +0100

    Always subclass so stubbed methods don't persist.

--- bendy.rb
+++ bendy.rb
@@ -1,7 +1,7 @@
 module Bendy
   module Shape
     def shape(*args)
-      klass = args[0].kind_of?(Class) ? args.shift : Class.new
+      klass = args[0].kind_of?(Class) ? Class.new(args.shift) : Class.new
       methods = args[-1].kind_of?(Hash) ? args.pop : {}
       methods.each do |attribute, value|
         proc = value.kind_of?(Proc) ? value : Proc.new { |*args| value }
