74a00f31e46c5b30a6aaaa3347df17f8e69f5baf
Manfred Stienstra <manfred@fngtps.com>
2019-03-12 09:29:52 +0100

    Modernize code for Ruby 2.6.

--- bendy.rb
+++ bendy.rb
@@ -1,10 +1,37 @@
 module Bendy
+  module Inspect
+    def self.prepended(base)
+      base.send(:extend, Bendy::Inspect::ClassMethods)
+    end
+
+    def __inspect_methods
+      if self.class.__config.nil?
+        ''
+      else
+        out = ' '
+        for method, value in self.class.__config
+          out << method.to_s + ':' + value.inspect + ', '
+        end
+        out[0..-3]
+      end
+    end
+
+    def inspect
+      "#<Bendy::Shape#{__inspect_methods}>"
+    end
+
+    module ClassMethods
+      attr_accessor :__config
+    end
+  end
+
   module Shape
-    def shape(*args)
-      klass = args[0].kind_of?(Class) ? Class.new(args.shift) : Class.new
-      methods = args[-1].kind_of?(Hash) ? args.pop : {}
+    def shape(*args, **methods)
+      klass = args[0].is_a?(Class) ? Class.new(args.shift) : Class.new
+      klass.send(:prepend, Bendy::Inspect)
+      klass.__config = methods
       methods.each do |attribute, value|
-        proc = value.kind_of?(Proc) ? value : Proc.new { |*args| value }
+        proc = value.is_a?(Proc) ? value : proc { |*_args| value }
         klass.send(:define_method, attribute, &proc)
       end
       klass.new(*args)
--- bendy_test.rb
+++ bendy_test.rb
@@ -1,30 +1,30 @@
-$:.unshift(File.expand_path('../', __FILE__))
+$LOAD_PATH.unshift(File.expand_path(__dir__))
 
-require 'test/unit'
+require 'minitest/autorun'
 require 'bendy'
 
-class Test::Unit::TestCase
+class Minitest::Test
   def self.test(description, &block)
-    define_method("test] #{description}", &block)
+    define_method("test_ #{description}", &block)
   end
 end
 
-class BendyShapeTest < Test::Unit::TestCase
+class BendyShapeTest < Minitest::Test
   include Bendy::Shape
-  
+
   def setup
     @object = shape(
-      :name => 'Appie',
-      :length => 42
+      name: 'Appie',
+      length: 42
     )
   end
-  
-  test "invokes stubbed accessors" do
+
+  test 'invokes stubbed accessors' do
     assert_equal 'Appie', @object.name
     assert_equal 42, @object.length
   end
-  
-  test "responds to stubbed accessors" do
+
+  test 'responds to stubbed accessors' do
     assert @object.respond_to?(:name)
     assert @object.respond_to?(:length)
   end
@@ -32,81 +32,83 @@ end
 
 class Cow
   attr_reader :color
-  
-  def initialize(color=nil)
+
+  def initialize(color = nil)
     @color = color
   end
-  
+
   def moo
     'moo'
   end
 end
 
-class BendyShapeSubclassTest < Test::Unit::TestCase
+class BendyShapeSubclassTest < Minitest::Test
   include Bendy::Shape
-  
+
   def setup
-    @object = shape(Cow,
-      :name => 'Appie',
-      :length => 42
+    @object = shape(
+      Cow,
+      name: 'Appie',
+      length: 42
     )
   end
-  
-  test "invokes stubbed accessors" do
+
+  test 'invokes stubbed accessors' do
     assert_equal 'Appie', @object.name
     assert_equal 42, @object.length
   end
-  
-  test "responds to stubbed accessors" do
+
+  test 'responds to stubbed accessors' do
     assert @object.respond_to?(:name)
     assert @object.respond_to?(:length)
   end
 end
 
-class BendyShapeLambdaTest < Test::Unit::TestCase
+class BendyShapeLambdaTest < Minitest::Test
   include Bendy::Shape
-  
+
   def setup
-    @object = shape(Cow,
-      :name => lambda { 'Appie' },
-      :[] => lambda { |index| index }
+    @object = shape(
+      Cow,
+      name: -> { 'Appie' },
+      :[] => ->(index) { index }
     )
   end
-  
-  test "invokes stubbed accessors" do
+
+  test 'invokes stubbed accessors' do
     assert_equal 'moo', @object.moo
     assert_equal 'Appie', @object.name
     assert_equal 42, @object[42]
   end
-  
-  test "responds to stubbed accessors" do
+
+  test 'responds to stubbed accessors' do
     assert @object.respond_to?(:moo)
     assert @object.respond_to?(:name)
     assert @object.respond_to?(:[])
   end
 end
 
-class BendyShapeSubclassWithArgsTest < Test::Unit::TestCase
+class BendyShapeSubclassWithArgsTest < Minitest::Test
   include Bendy::Shape
-  
+
   def setup
-    @object = shape(Cow,
+    @object = shape(
+      Cow,
       'Green',
-      :name => 'Appie',
-      :length => 42
+      name: 'Appie',
+      length: 42
     )
   end
-  
-  test "passes arguments to the initializer" do
+
+  test 'passes arguments to the initializer' do
     assert_equal 'Green', @object.color
   end
 end
 
-
-class BendyShapeExceptionTest < Test::Unit::TestCase
+class BendyShapeExceptionTest < Minitest::Test
   include Bendy::Shape
-  
-  test "tries to pass regular arguments" do
+
+  test 'tries to pass regular arguments' do
     assert_raises(ArgumentError) do
       shape('Green')
     end
